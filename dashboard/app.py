eb6c9f0af4571c9743cefb723eb0a42d4facdb42
Mario Vilchesgit config --global user.name Mario <mvilchesp@udec.cl>
2022-08-08 17:42:26 -0400

    modified:   dashboard/app.py 	new file:   dashboard/data.py 	new file:   dashboard/gapminderData2.csv 	new file:   dashboard/scatter.py 	modified:   docker-compose.yml

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,5 +1,7 @@
-from dash import Dash, html
+from dash import Dash, html, dcc
 import os
+from data import gapminder
+from scatter import get_scatter
 
 is_gunicorn = "gunicorn" in os.environ.get("SERVER_SOFTWARE", "")
 if is_gunicorn:
@@ -11,8 +13,12 @@ else:
 app = Dash(__name__, requests_pathname_prefix=requests_pathname_prefix)
 server = app.server
 
-app.layout = html.Div(children="Hello World")
-
+app.layout = html.Div(
+    children=[
+        html.H1("Ejercicio 1"),
+        dcc.Graph(id="ejercicio1", figure=get_scatter(gapminder, 2007)),
+    ]
+)
 
 if __name__ == "__main__":
-    app.run_server(debug=True, host="0.0.0.0", port="5000")
+    app.run_server(debug=True, host="0.0.0.0", port="5050")
---
+++ dashboard/data.py
@@ -0,0 +1,3 @@
+import pandas as pd
+data_url = "https://raw.githubusercontent.com/holtzy/The-Python-Graph-Gallery/master/static/data/gapminderData.csv"
+gapminder = pd.read_csv(data_url)
